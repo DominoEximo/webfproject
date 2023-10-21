9ff744d480af490847102ac6e01c6a91366049a3
PappDávid <PappDavid32@gmail.com>
2023-10-21 15:40:00 +0200

    created repo

---
+++ src/test/java/hu/project/webfproject/WebfprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.project.webfproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
