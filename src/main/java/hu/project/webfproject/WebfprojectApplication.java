35177aafeec5b8925423f9de890182e28bde23d5
PappDávid <PappDavid32@gmail.com>
2023-10-21 16:39:11 +0200

    added dependencies to be able to use jsf

--- src/main/java/hu/project/webfproject/WebfprojectApplication.java
+++ src/main/java/hu/project/webfproject/WebfprojectApplication.java
@@ -3,6 +3,7 @@ package hu.project.webfproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WebfprojectApplication {
 
@@ -11,3 +12,4 @@ public class WebfprojectApplication {
     }
 
 }
+
